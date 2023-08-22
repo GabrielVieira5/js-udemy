20b16ea90fc94ec33d5a00579c5687f029747d99
GabrielVieira5 <gabrielg8820@gmail.com>
2023-08-22 19:59:52 -0300

    eutoacabado

---
+++ aulas/aula026/math.js
@@ -0,0 +1,10 @@
+let num1 = 2.523;
+let num2 = 4.5
+
+
+console.log(Math.floor(num1));
+console.log(Math.ceil(num1));
+console.log(Math.round(num1));
+
+num1 = Math.random();
+console.log(num1)
---
+++ exercicios/numbers-028/miau.js
@@ -0,0 +1,6 @@
+const num1 = Number(prompt('Digite o seu número: '));
+const num1tit = document.getElementById('num-prin');
+
+num1tit = document.body.innerHTML = num1
+
+//document.body.innerHTML = num1
