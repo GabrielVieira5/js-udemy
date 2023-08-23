943e2bd349407dc9bc627ef53ec3077e184070e9
GabrielVieira5 <gabrielg8820@gmail.com>
2023-08-23 15:17:40 -0300

    yeye

--- exercicios/numbers-028/miau.js
+++ exercicios/numbers-028/miau.js
@@ -1,6 +1,12 @@
 const num1 = Number(prompt('Digite o seu número: '));
 const num1tit = document.getElementById('num-prin');
+const texto = document.getElementById('texto1')
 
-num1tit = document.body.innerHTML = num1
+num1tit.innerHTML = num1;
+texto.innerHTML += `Seu número mais 2 é ${num1+2} <br>`;
+texto.innerHTML += `Seu número menos 2 é ${num1-2} <br>`;
+texto.innerHTML += `Seu número arredondado para baixo é: ${Math.floor(num1)} <br>`;
+texto.innerHTML += `Seu número arredondado para cima é: ${Math.ceil(num1)} <br>`;
+
 
 //document.body.innerHTML = num1
