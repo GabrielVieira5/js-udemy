26ceb302fb990f6a7678f55ba50950da7a076038
GabrielVieira5 <gabrielg8820@gmail.com>
2023-08-23 21:29:33 -0300

    arrays, m

---
+++ aulas/aula031/funcoes.js
@@ -0,0 +1,16 @@
+function soma(x, y){
+    const result = x + y
+    return  result
+}
+function sub(x, y){
+    const result = x - y;
+    return result;
+}
+
+console.log(sub(5, 2));
+
+//outra forma de criar functon: 
+
+const soma2 = (n, n2) => n+n2
+
+console.log(soma2 (16, 10));
