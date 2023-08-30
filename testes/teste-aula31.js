eba97d62454d5d623d8397d2faf6418f9ecc770d
GabrielVieira5 <gabrielg8820@gmail.com>
2023-08-30 16:41:21 -0300

    im not finhsed

---
+++ testes/teste-aula31.js
@@ -0,0 +1,13 @@
+const gabriel = {
+    sobrenome: 'Vieira',
+    idade: '17',
+    anonascimento: (this.idade)-2023
+}
+
+console.log(`O Ano de nascimento do usuário é ${gabriel.Anonascimento}`)
+
+function falar() {
+    return console.log('Vieira é programador')
+}
+
+falar();
