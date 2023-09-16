5d09883ad498de2b743af893563881a878894c73
GabrielVieira5 <gabrielg8820@gmail.com>
2023-09-16 08:58:22 -0300

    aaaaaaaaaaa

--- aulas/aula44/calculo.js
+++ aulas/aula44/calculo.js
@@ -1,13 +1,32 @@
 const form = document.querySelector('.formulario');
 
-form.addEventListener('submit', function (e){
+form.addEventListener('submit', function teste (e){
     e.preventDefault();
+    
+    const inputPeso = e.target.querySelector('#input-teste-4');
+    const inputAltura = e.target.querySelector('#input-teste-3');
+    const peso = (Number(inputPeso.value));
+    const altura = (Number(inputAltura.value));
+
+    if (!peso){
+        return console.log('Peso inválido')
+    } else if (!altura){
+        console.log('Altura inválida')
+    }
+    return console.log('deu certo')
 });
+
+function criar (){
+    const p = createElement('p'); //vai criar o elemento que está dentro de ()
+
+}
+
 function setResultado (result){
     const resultado = document.querySelector('#resultado');
     resultado.innerHTML = (''); //LIMPA O HTML DESSA PARTE
-    const p = document.createElement('p'); //CRIA UM ELEMENTO DENTRO DE HTML MAS SEM DIRECIONA-LO
     p.innerHTML = 'Legal'; //Adiona conteudo ao elemento
     resultado.appendChild(p); //diz que o elemento que esta entre () é filho (esta dentro) do que foi selecionado que no caso é RESULTADO
     
 }
+
+teste();
