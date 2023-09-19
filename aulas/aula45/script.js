d367f06c0cb14458826480032802ff0deda13702
GabrielVieira5 <gabrielg8820@gmail.com>
2023-09-19 20:42:55 -0300

    aaaa

---
+++ aulas/aula45/script.js
@@ -0,0 +1,16 @@
+//Método de substituição de IF e ELSE CURTOS:
+
+//MÉTUDO ENCURTADO:
+const pontuacao = 1500;
+const status = pontuacao >= 1000 ? 'Usúario VIP' : 'Usúario';
+
+console.log(status)
+
+/*
+MÉTODO NORMAL
+const pontuacao = 1500;
+if (pontuacao >= 1000){
+    console.log('Usuário VIP :)');
+} else{
+    console.log('Normal');
+}*/
---
+++ aulas/aula46/script.js
@@ -0,0 +1,2 @@
+const data = new Date(2005, 10 , 10, 15, 47, 12, 10); //cria uma várivael de tipo data
+console.log(data.toString());
