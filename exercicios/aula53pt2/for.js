6cf771a1955105622dd964e2cd0ab8d031b8e349
Gabriel Vieira <gabrielg8820@gmail.com>
2023-12-07 12:25:33 -0300

    ÇÇÇÇ

--- exercicios/aula53pt2/for.js
+++ exercicios/aula53pt2/for.js
@@ -5,16 +5,19 @@ const elementos = [
     {tag: 'section', texto: 'Frase 4'} //3
 ];
 
-const container = document.querySelector('.container');
-const div = document.createElement('div');
+const container = document.querySelector('.container'); //seleciona o que tem a classe container no html
+const div = document.createElement('div'); //cria o elemento div mas nao o adiciona
 
-for(let i = 0; i < elementos.length; i++){
-    let {tag, texto} = elementos[i];
-    let tagCriada = document.createElement(tag);
-    let textoCriado = document.createTextNode(texto);
-    tagCriada.appendChild(textoCriado);
-    div.appendChild(tagCriada);
+for(let i = 0; i < elementos.length; i++){ 
+    let {tag, texto} = elementos[i]; //seleciona os objetos tag e texto, dentro de elementos no array do numero definido pelo for (i)
+    let tagCriada = document.createElement(tag); //cria o elemento selecionado no momento pela configuração da linha de cima
+    let textoCriado = document.createTextNode(texto); //pega o texto e altera como innerHTML, tambem funciona com innerText, creatTextNode(como é o caso)
+    
+    //em outra forma como innerText, seria: 
+    //tagCriada.innerText = texto; (CASO FOSSE USADO ESSE, NAO PRECISARIA DA LINHA ABAIXO)
+    tagCriada.appendChild(textoCriado); //define o texto acima como filho de tagCriada
+    div.appendChild(tagCriada); //define tagCriada como filho de div
     
 }
 
-container.appendChild(div);
+container.appendChild(div); //define div como filho de containers
