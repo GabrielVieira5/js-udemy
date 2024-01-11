4650e9644d50d8d9b04da0f98470ea391f9a1db2
Gabriel Vieira <gabrielg8820@gmail.com>
2024-01-11 10:39:36 -0300

    aaaa

---
+++ super/revisao/exercicio-teste/script.js
@@ -0,0 +1,8 @@
+function machine () {
+    const form = document.querySelector('.form');
+
+    form.addEventListener('submit', function recebeEventoForm (evento));
+
+
+}
+machine();
---
+++ super/revisao/funcoes.js
@@ -0,0 +1,10 @@
+/*function machine (num1, num2){
+    const resultado = num1 + num2;
+    return `A soma entre ${num1} e ${num2} é igual a: ${resultado}`;
+}
+
+console.log(machine('vieira', 'giovanna'))*/
+
+const raiz = num1 => num1 ** 0.5;
+
+console.log(raiz(25));
