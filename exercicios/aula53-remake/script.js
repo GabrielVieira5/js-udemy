af743fefe3ad6f6e415596af345591a0391e979c
Gabriel Vieira <gabrielg8820@gmail.com>
2024-01-17 09:40:42 -0300

    daoraaa

--- exercicios/aula53-remake/script.js
+++ exercicios/aula53-remake/script.js
@@ -5,14 +5,49 @@ const elementos = [
     {tag: 'div', texto: 'Formula1'} //3
 ]
 
-function machine(){
-    const area = document.querySelector('.container');
-    
-    for(let i = 0; i < elementos.length; i++){
-        console.log(elementos[i].tag)
-        console.log(elementos[i].texto)
-        
-    }
-}
 
-machine()
+const container = document.querySelector('.container');
+//querySelector seleciona so um e com um "All" no fim seleciona todos
+const div = document.createElement('div');
+
+for(let i = 0; i < elementos.length; i++){
+    console.log(elementos[i]);
+    let {tag, texto} = elementos[i]; //via desustruturação
+    let tagCriada = document.createElement(tag);
+    tagCriada.innerText = texto;
+    div.appendChild(tagCriada);
+
+}
+container.appendChild(div);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function machine(){
+//     const area = document.querySelector('.container');
+    
+//     for(let i = 0; i < elementos.length; i++){
+//         console.log(elementos[i].tag);
+//         console.log(elementos[i].texto);
+        
+//         area.innerHTML += `${elementos[i].tag} ${elementos[i].texto} <br>`
+
+        
+//     }
+// }
+
+// machine()
